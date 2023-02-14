547403a81ab84fb2ffdd662d4c442b05a720ce08
Samarth H Chinivar <samarthharish321@gmail.com>
2023-02-14 16:48:21 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// using class based compnents here
+class Counter extends React.Component {
+  //initial state of counter
+  state = {
+    counter: 0
+  }
+  
+  //function to update state of counter
+  increment = () => {
+    this.setState({counter: this.state.counter+1});
+    navigator.vibrate(300); //vibrates after user clicks this button
+
+  }
+
+  //function which renders whole content in class based components
+  render() {
+    return <div className='main'>
+      <div className='header'>
+        <h1>Counter Application</h1>
+        <br/>
+        <h4>Please do use this counter app in landscape mode for better viewing &#128151; &#128151;</h4>
+      </div>
+      
+      <div className='container'>
+        <section className='value'>
+          <p class='font-weight-bold'>{this.state.counter}</p> 
+          {/*shows value of counter */}
+        </section>
+
+        <section className='button'> 
+          <button type='button' onClick={this.increment} class='btn btn-primary btn-lg'>Increment</button> 
+          {/**button component and it's after effects */}
+        </section>
+      </div>
+
+      <div className='contact'>
+        <a href='https://samarth-portfolio-website.000webhostapp.com' target='_blank'>
+          <button type='button' class='btn btn-success'>Contact</button>
+        </a>
+      </div>
+
+    </div> 
+  }
+}
+
+const el = <Counter />; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  el, 
+  document.getElementById('root')
+);
